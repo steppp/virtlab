0c10e6c76df53b7a30885ec3bd496b58db318fa1
Stefano Andriolo <stefano.andriolo3@studio.unibo.it>
2020-07-09 15:32:51 +0200

    Add first stub for fcntl virtualization

--- fcntlflock/dlflock.c
+++ fcntlflock/dlflock.c
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-// define the type of the original function to be used with dlsym
+// define the type of the original flock function to be used with dlsym
 typedef int (*real_flock)(int, int);
+// define the type of the original fcntl function to be used with dlsym
+typedef int (*real_fcntl)(int, int, ...);
 
 // override the flock SC
 int flock(int fd, int operation) {
@@ -29,6 +32,38 @@ int flock(int fd, int operation) {
 			return EINVAL;
 	}
 
+	printf("lock\n");
+
 	return ((real_flock)dlsym(RTLD_NEXT, "flock"))(fd, operation);
 }
 	
+int fcntl(int fd, int cmd, ...) {
+	va_list ap;
+	struct flock *lockinfo;
+	void *argp;
+	int res;
+
+	// get the actual fcntl function address
+	real_fcntl r_fcntl = (real_fcntl) dlsym(RTLD_NEXT, "fcntl");
+	va_start(ap, cmd);
+
+	switch (cmd) {
+		case F_SETLK:
+		case F_SETLKW:
+		case F_GETLK:
+			// retrueve the flock struct pointer from the variadic parameter
+			lockinfo = va_arg(ap, struct flock *);
+			printf("fcntl called to manage record locking\n");
+			res = r_fcntl(fd, cmd, lockinfo);
+			break;
+		default:
+			// same as above
+			argp = va_arg(ap, void *);
+			res = r_fcntl(fd, cmd, argp);
+			break;
+	}
+
+	va_end(ap);
+	return res;
+}
+
